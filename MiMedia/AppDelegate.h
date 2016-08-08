79349c4af326a94bcd677034b1883a4df2390f65
iSquare5 <isquare5@isquare5s-mac-pro.local>
2016-08-08 13:05:05 +0530

    Initial Commit

---
+++ MiMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiMedia
+//
+//  Created by iSquare5 on 08/08/16.
+//  Copyright © 2016 MitSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiMedia
+//
+//  Created by iSquare5 on 08/08/16.
+//  Copyright © 2016 MitSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
